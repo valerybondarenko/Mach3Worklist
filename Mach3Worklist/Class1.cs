dc5254302266ee959e9ac4d914ba8b7cb467dc0a
Valery Bondarenko <bondarenkovalery@yandex.ru>
2025-01-23 17:29:03 +0300

    token class

---
+++ Mach3Worklist/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mach3Worklist
+{
+    internal class Token
+    {
+        enum 
+
+        public Token() { }
+
+        public string Head { get; set; } = string.Empty;
+        public string Tail { get; set; } = string.Empty;
+
+
+    }
+}
